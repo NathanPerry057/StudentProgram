6f9f4cb5afcc27613f100bd5aeaac1eba182bc83
NathanPerry057 <G00394355@atu.ie>
2022-10-28 02:02:44 +0100

    Added a constructor and variables for student info

---
+++ src/main/java/ie/atu/student.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+public class student {
+    private String name;
+    private String email;
+    private String course;
+
+    public static void main(String [] args) {
+        System.out.println("");
+
+    }
+
+}
