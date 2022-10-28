42069060bc128a7bda1d2aa353f6850b434c1ce4
NathanPerry057 <G00394355@atu.ie>
2022-10-28 02:04:51 +0100

    Imported and coded scanner

--- src/main/java/ie/atu/student.java
+++ src/main/java/ie/atu/student.java
@@ -1,9 +1,13 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class student {
     private String name;
     private String email;
     private String course;
+
+    Scanner s = new Scanner(System.in);
 
     public static void main(String [] args) {
         System.out.println("");
